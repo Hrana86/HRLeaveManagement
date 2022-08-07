2422cb547aba6db0f3b5f52d3afb8c959aca3b89
Josip Hranilović <josip@zelenetehnologije.hr>
2022-08-07 08:28:14 +0200

    API

---
+++ HRLeaveManagement.Persistence/Configurations/Entities/LeaveAllocationConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using HRLeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HRLeaveManagement.Persistence.Configurations.Entities;
+public class LeaveAllocationConfiguration : IEntityTypeConfiguration<LeaveAllocation>
+{
+    public void Configure(EntityTypeBuilder<LeaveAllocation> builder)
+    {
+
+    }
+}
---
+++ HRLeaveManagement.Persistence/LeaveManagementDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HRLeaveManagement.Persistence;
+public class LeaveManagementDbContextFactory : IDesignTimeDbContextFactory<LeaveManagmentDbContext>
+{
+    public LeaveManagmentDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<LeaveManagmentDbContext>();
+        var connectionString = configuration.GetConnectionString("Default");
+
+        builder.UseSqlServer(connectionString);
+
+        return new LeaveManagmentDbContext(builder.Options);
+    }
+}
